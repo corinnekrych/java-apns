1bc8a83bbd09bf1a48ecfe3c3997b6f69d9003a9
corinne krych <corinnekrych@gmail.com>
2014-11-18 15:05:02 +0100

    2 test cases

--- src/test/java/com/notnoop/apns/APNSTest.java
+++ src/test/java/com/notnoop/apns/APNSTest.java
@@ -28,10 +28,10 @@ public class APNSTest {
 
 
     @Test
-    public void MY_TEST_FOR_WRONG_CERTFIFICATE() {
+    public void MY_TEST_FOR_ONE_DEVICE_WITH_CORRECT_CERTFIFICATE() {
 ApnsService service =
     APNS.newService()
-        .withCert("/Users/corinne/Documents/UPS_Documents/Certificates_Prod.p12", "XXX")
+        .withCert("/Users/corinne/Documents/UPS_Documents/Certificates_Prod.p12", "password")
         .withProductionDestination()
         .withDelegate(new ApnsDelegate() {
             @Override
@@ -62,20 +62,65 @@ ApnsService service =
         .build();
 
 
-String payload = APNS.newPayload().alertBody("with ONE VALID.....RECEIVED").build();
-ArrayList<String> mixedTokens = new ArrayList();
-//mixedTokens.add("54668ba370d79ad065de03d02627e8d035b2ce499ce5322fd1041de6c6c3d254");
-mixedTokens.add("9d860e6ec706611db76afb419cee897ffb3af1317b8b90ded4196c9044e4087e");
+        String payload = APNS.newPayload().alertBody("1. with ONE VALID sep test").build();
+        ArrayList<String> mixedTokens = new ArrayList();
+        //correct device
+        mixedTokens.add("9d860e6ec706611db76afb419cee897ffb3af1317b8b90ded4196c9044e4087e");
 
-service.push(mixedTokens, payload);   
+        service.push(mixedTokens, payload);   
+
+        String payload2 = APNS.newPayload().alertBody("1. with BOTH IN FIRST TEST").build();
+        ArrayList<String> mixedTokens2 = new ArrayList();
+        mixedTokens2.add("54668ba370d79ad065de03d02627e8d035b2ce499ce5322fd1041de6c6c3d254");
+        mixedTokens2.add("9d860e6ec706611db76afb419cee897ffb3af1317b8b90ded4196c9044e4087e");
+
+        service.push(mixedTokens2, payload2); 
 
 
-String payload2 = APNS.newPayload().alertBody("with BOTH.....NOT RECEIVED").build();
-ArrayList<String> mixedTokens2 = new ArrayList();
-mixedTokens2.add("54668ba370d79ad065de03d02627e8d035b2ce499ce5322fd1041de6c6c3d254");
-mixedTokens2.add("9d860e6ec706611db76afb419cee897ffb3af1317b8b90ded4196c9044e4087e");
+    }
 
-service.push(mixedTokens2, payload2);    
+        @Test
+    public void MY_TEST_FOR_TWO_DEVICES_ONE_IS_WRONG_CERTFIFICATE() {
+ApnsService service =
+    APNS.newService()
+        .withCert("/Users/corinne/Documents/UPS_Documents/Certificates_Prod.p12", "password")
+        .withProductionDestination()
+        .withDelegate(new ApnsDelegate() {
+            @Override
+            public void messageSent(ApnsNotification message, boolean resent) {
+                System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::>>>messageSent");
+            }
 
-}
+            @Override
+            public void messageSendFailed(ApnsNotification message, Throwable e) {
+                System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::>>>messageSendFailed" + message.getDeviceToken().toString());
+            }
+
+            @Override
+            public void connectionClosed(DeliveryError e, int messageIdentifier) {
+                 System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::>>>Closed: " + e);
+            }
+
+            @Override
+            public void cacheLengthExceeded(int newCacheLength) {
+                System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::>>>cacheLengthExceeded");
+            }
+
+            @Override
+            public void notificationsResent(int resendCount) {
+                 System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::>>>Retry: " + resendCount);
+            }
+        })
+        .build();
+
+
+
+        String payload2 = APNS.newPayload().alertBody("2. with BOTH, sep test").build();
+        ArrayList<String> mixedTokens2 = new ArrayList();
+        mixedTokens2.add("54668ba370d79ad065de03d02627e8d035b2ce499ce5322fd1041de6c6c3d254");
+        mixedTokens2.add("9d860e6ec706611db76afb419cee897ffb3af1317b8b90ded4196c9044e4087e");
+
+        service.push(mixedTokens2, payload2);    
+
+    }
 }
